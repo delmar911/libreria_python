8679f69be56f9e829490b087bae60eab51b46d26
Maria Del Mar <mariadelmar.artunduaga911@gmail.com>
2024-06-12 13:45:08 -0500

    Libro ya enlista y registra

--- backend/libreria/urls.py
+++ backend/libreria/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path,include
-
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 
 from libreria import views
@@ -14,5 +14,6 @@ router_usuarios.register(r'usuarios', views.UsuarioView)
 urlpatterns = [
     # Incluir las URLs de los enrutadores correspondientes en los patrones de URL
     path("api/v1/", include(router_libros.urls)),
+    path("docs/", include_docs_urls(title="Libreria Api")),
     path("api/v1/", include(router_usuarios.urls)),
 ]
